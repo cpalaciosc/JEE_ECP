5ca8772f2019a06cdc8a22b8a10fbe622dbf4af6
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-04 13:52:42 +0100

    Update NivelEstudio - Agregar nuevos niveles de estudio

--- src/main/java/es/upm/miw/models/utils/NivelEstudio.java
+++ src/main/java/es/upm/miw/models/utils/NivelEstudio.java
@@ -1,6 +1,12 @@
 package es.upm.miw.models.utils;
 
 public enum NivelEstudio {
-    GRADO, MASTER, DOCTORADO;
+    PRIMARIA(1), SECUNDARIA(2), GRADO(3), MASTER(4), DOCTORADO(5);
+    
+    public int valor;
+    
+    private NivelEstudio(int valor){
+        this.valor = valor;
+    }
 
 }
