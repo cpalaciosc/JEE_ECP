1e94b6266d6e585611387319a2bd0971d193c2a4
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-05 10:35:13 +0100

    Add TemaDaoJpa

---
+++ src/main/java/es/upm/miw/models/daos/jpa/TemaDaoJpa.java
@@ -0,0 +1,12 @@
+package es.upm.miw.models.daos.jpa;
+
+import es.upm.miw.models.daos.TemaDao;
+import es.upm.miw.models.entities.Tema;
+
+public class TemaDaoJpa extends GenericDaoJpa<Tema, Integer> implements TemaDao{
+
+    public TemaDaoJpa() {
+        super(Tema.class);
+    }
+
+}
