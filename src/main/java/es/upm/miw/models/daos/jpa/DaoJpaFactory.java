5a4b24369b2486bb539dee66d979d1884bf627c8
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-05 10:34:49 +0100

    Add DaoJpaFactory

---
+++ src/main/java/es/upm/miw/models/daos/jpa/DaoJpaFactory.java
@@ -0,0 +1,36 @@
+package es.upm.miw.models.daos.jpa;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.apache.logging.log4j.LogManager;
+
+import es.upm.miw.models.daos.DaoFactory;
+import es.upm.miw.models.daos.TemaDao;
+import es.upm.miw.models.daos.VotacionDao;
+
+public class DaoJpaFactory extends DaoFactory {
+    private static final String PERSISTENCE_UNIT = "JEE_ECP";
+
+    private static EntityManagerFactory entityManagerFactory = Persistence
+            .createEntityManagerFactory(PERSISTENCE_UNIT);
+
+    public DaoJpaFactory() {
+        LogManager.getLogger(DaoJpaFactory.class).debug("create Entity Manager Factory");
+    }
+
+    public static EntityManagerFactory getEntityManagerFactory() {
+        return entityManagerFactory;
+    }
+
+    @Override
+    public TemaDao getTemaDao() {
+        return new TemaDaoJpa();
+    }
+
+    @Override
+    public VotacionDao getVotacionDao() {
+        return new VotacionDaoJpa();
+    }
+
+}
