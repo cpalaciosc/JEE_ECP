4b8cbf60d740fa4792a4a09ea805d3306e30f271
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-05 10:35:34 +0100

    Add VotacionDaoJpa

---
+++ src/main/java/es/upm/miw/models/daos/jpa/VotacionDaoJpa.java
@@ -0,0 +1,12 @@
+package es.upm.miw.models.daos.jpa;
+
+import es.upm.miw.models.daos.VotacionDao;
+import es.upm.miw.models.entities.Votacion;
+
+public class VotacionDaoJpa extends GenericDaoJpa<Votacion, Integer> implements VotacionDao {
+
+    public VotacionDaoJpa() {
+        super(Votacion.class);
+    }
+
+}
