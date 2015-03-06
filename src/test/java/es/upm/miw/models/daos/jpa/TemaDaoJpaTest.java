45609b35eba900b0a0bbee1dbac391697d29b96c
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-06 19:03:50 +0100

    Update TemaDaoJPaTest - Pruebas del Test aun no funciona que la base de datos quede limpia luego de cada test

--- src/test/java/es/upm/miw/models/daos/jpa/TemaDaoJpaTest.java
+++ src/test/java/es/upm/miw/models/daos/jpa/TemaDaoJpaTest.java
@@ -1,10 +1,10 @@
 package es.upm.miw.models.daos.jpa;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import org.apache.logging.log4j.LogManager;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -17,52 +17,62 @@ public class TemaDaoJpaTest {
     private TemaDao dao;
 
     private Tema tema;
-
+    
     @BeforeClass
     public static void beforeClass() {
         LogManager.getLogger(TemaDaoJpaTest.class).debug(
                 "Vaciando la base de datos antes de empezar - drop and create tables");
         DaoFactory.setFactory(new DaoJpaFactory());
-        DaoJpaFactory.dropAndCreateTables();
     }
 
     @Before
     public void before() {
+        LogManager.getLogger(TemaDaoJpaTest.class).debug("Inicio de before()");      
         this.tema = new Tema(1, "Tema1", "Pregunta1");
         dao = DaoFactory.getFactory().getTemaDao();
         dao.create(tema);
+        LogManager.getLogger(TemaDaoJpaTest.class).debug("Fin de before()");     
     }
 
     @Test
     public void testRead() {
+        LogManager.getLogger(TemaDaoJpaTest.class).debug("Inicio de testRead()");
         assertEquals(tema, dao.read(tema.getId()));
+        LogManager.getLogger(TemaDaoJpaTest.class).debug("Fin de testRead()");
     }
 
     @Test
     public void testUpdate() {
+        LogManager.getLogger(TemaDaoJpaTest.class).debug("Inicio de testUpdate()");
         this.tema.setPregunta("Pregunta2");
         dao.update(this.tema);
         assertEquals(tema.getPregunta(), dao.read(tema.getId()).getPregunta());
+        LogManager.getLogger(TemaDaoJpaTest.class).debug("Fin de testUpdate()");
     }
 
     @Test
     public void testDeleteByID() {
+        LogManager.getLogger(TemaDaoJpaTest.class).debug("Inicio de testDeleteByID()");
         dao.deleteById(tema.getId());
         assertNull(dao.read(tema.getId()));
+        LogManager.getLogger(TemaDaoJpaTest.class).debug("Fin de testDeleteByID()");
     }
 
     @Test
     public void testFindAll() {
+        LogManager.getLogger(TemaDaoJpaTest.class).debug("Inicio de testFindAll()");
         dao.create(new Tema(2, "Tema2", "Pregunta2"));
         dao.create(new Tema(3, "Tema3", "Pregunta3"));
         assertEquals(3, dao.findAll().size());
+        LogManager.getLogger(TemaDaoJpaTest.class).debug("Fin de testFindAll()");
     }
 
+    
     @After
     public void after() {
         LogManager.getLogger(TemaDaoJpaTest.class).debug(
                 "Vaciando la base de datos antes de salir - drop and create tables");
-        DaoJpaFactory.dropAndCreateTables();
+        dao.deleteAll();
     }
-
+    
 }
