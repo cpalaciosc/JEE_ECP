934f3fe28c8f95102e0a5781fc2589e1606f235b
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-08 23:04:33 +0100

    Update NivelEstudio

--- src/main/java/es/upm/miw/models/utils/NivelEstudio.java
+++ src/main/java/es/upm/miw/models/utils/NivelEstudio.java
@@ -1,12 +1,6 @@
 package es.upm.miw.models.utils;
 
 public enum NivelEstudio {
-    PRIMARIA("P"), SECUNDARIA("S"), GRADO("G"), MASTER("M"), DOCTORADO("D");
-    
-    public String valor;
-    
-    private NivelEstudio(String valor){
-        this.valor = valor;
-    }
+    PRIMARIA, SECUNDARIA, GRADO, MASTER, DOCTORADO;
 
 }
