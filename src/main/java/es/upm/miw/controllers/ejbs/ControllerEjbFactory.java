3c6c398ccd517cf7578f686195a23a9642ab5a72
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-09 13:53:20 +0100

    Update ControllerEjbFactory

--- src/main/java/es/upm/miw/controllers/ejbs/ControllerEjbFactory.java
+++ src/main/java/es/upm/miw/controllers/ejbs/ControllerEjbFactory.java
@@ -4,9 +4,37 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 import es.upm.miw.controllers.ControllerFactory;
+import es.upm.miw.controllers.IEliminarTemaController;
+import es.upm.miw.controllers.IIncorporarTemaController;
+import es.upm.miw.controllers.IVerVotacionesController;
+import es.upm.miw.controllers.IVotarController;
 
 @ManagedBean(name = "controllerFactory")
 @SessionScoped
 public class ControllerEjbFactory extends ControllerFactory {
+
+    @Override
+    public IEliminarTemaController getEliminarTemaController() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public IIncorporarTemaController getIncorporarTemaController() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public IVerVotacionesController getVerVotacionesController() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public IVotarController getVotarController() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
