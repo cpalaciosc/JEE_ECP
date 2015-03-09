6027ebbeb1b25ba904e27cc3678b6445f633c747
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-09 13:32:23 +0100

    Add EliminarTemaController

---
+++ src/main/java/es/upm/miw/controllers/ejbs/EliminarTemaController.java
@@ -0,0 +1,7 @@
+package es.upm.miw.controllers.ejbs;
+
+import es.upm.miw.controllers.IEliminarTemaController;
+
+public class EliminarTemaController extends ControllerEjb implements IEliminarTemaController {
+
+}
