a39bea367cbbfd3e86f20c84ba72f5a316a6964c
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-09 13:52:59 +0100

    Update ControllerFactory

--- src/main/java/es/upm/miw/controllers/ControllerFactory.java
+++ src/main/java/es/upm/miw/controllers/ControllerFactory.java
@@ -1,5 +1,10 @@
 package es.upm.miw.controllers;
 
 public abstract class ControllerFactory {
+    
+    public abstract IEliminarTemaController getEliminarTemaController();
+    public abstract IIncorporarTemaController getIncorporarTemaController();
+    public abstract IVerVotacionesController getVerVotacionesController();
+    public abstract IVotarController getVotarController();
 
 }
