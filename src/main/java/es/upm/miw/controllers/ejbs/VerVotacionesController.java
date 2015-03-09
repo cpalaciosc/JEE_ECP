8415c12557797c7e824644cbf495f5e2695ad3c8
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-09 13:37:55 +0100

    Add VerVotacionesController

---
+++ src/main/java/es/upm/miw/controllers/ejbs/VerVotacionesController.java
@@ -0,0 +1,7 @@
+package es.upm.miw.controllers.ejbs;
+
+import es.upm.miw.controllers.IVerVotacionesController;
+
+public class VerVotacionesController extends ControllerEjb implements IVerVotacionesController {
+
+}
