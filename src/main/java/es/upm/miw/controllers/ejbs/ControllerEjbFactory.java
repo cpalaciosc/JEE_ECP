c7200848e54ad158618479cb6d5ebcef3b0d4449
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-09 13:28:54 +0100

    Add ControllerEjbFactory

---
+++ src/main/java/es/upm/miw/controllers/ejbs/ControllerEjbFactory.java
@@ -0,0 +1,12 @@
+package es.upm.miw.controllers.ejbs;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import es.upm.miw.controllers.ControllerFactory;
+
+@ManagedBean(name = "controllerFactory")
+@SessionScoped
+public class ControllerEjbFactory extends ControllerFactory {
+
+}
