b9ac7dad274eb2ff1dc24a66e988af43c02ef55e
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-09 21:08:20 +0100

    Update TemaDaoJpaTest

--- src/test/java/es/upm/miw/models/daos/jpa/TemaDaoJpaTest.java
+++ src/test/java/es/upm/miw/models/daos/jpa/TemaDaoJpaTest.java
@@ -10,71 +10,68 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import es.upm.miw.models.daos.DaoFactory;
-import es.upm.miw.models.daos.TemaDao;
+import es.upm.miw.models.daos.ITemaDao;
 import es.upm.miw.models.entities.Tema;
 
 public class TemaDaoJpaTest {
-    private TemaDao dao;
+    private ITemaDao dao;
 
     private Tema tema;
-    
+
     final static Class<TemaDaoJpaTest> clazz = TemaDaoJpaTest.class;
-    
+
     @BeforeClass
     public static void beforeClass() {
-        LogManager.getLogger(clazz).debug(
-                "Vaciando la base de datos antes de empezar - drop and create tables");
+        LogManager.getLogger(clazz).debug("Inicializando los test");
         DaoFactory.setFactory(new DaoJpaFactory());
     }
 
     @Before
     public void before() {
-        LogManager.getLogger(clazz).debug("Inicio de before()");      
-        this.tema = new Tema(1, "Tema1", "Pregunta1");
+        LogManager.getLogger(clazz).debug("Inicio de before()");
+        this.tema = new Tema("Tema1", "Pregunta1");
         dao = DaoFactory.getFactory().getTemaDao();
-        dao.create(tema);
-        LogManager.getLogger(clazz).debug("Fin de before()");     
+        dao.create(this.tema);
+        LogManager.getLogger(clazz).debug("Fin de before()");
     }
 
     @Test
     public void testRead() {
         LogManager.getLogger(clazz).debug("Inicio de testRead()");
-        assertEquals(tema, dao.read(tema.getId()));
+        assertEquals(this.tema, dao.read(this.tema.getId()));
         LogManager.getLogger(clazz).debug("Fin de testRead()");
     }
 
     @Test
     public void testUpdate() {
         LogManager.getLogger(clazz).debug("Inicio de testUpdate()");
-        this.tema.setPregunta("Pregunta2");
+        this.tema.setPregunta("Pregunta1 Actualizada");
         dao.update(this.tema);
-        assertEquals(tema.getPregunta(), dao.read(tema.getId()).getPregunta());
+        assertEquals(this.tema.getPregunta(), dao.read(this.tema.getId()).getPregunta());
         LogManager.getLogger(clazz).debug("Fin de testUpdate()");
     }
 
     @Test
     public void testDeleteByID() {
         LogManager.getLogger(clazz).debug("Inicio de testDeleteByID()");
-        dao.deleteById(tema.getId());
-        assertNull(dao.read(tema.getId()));
+        dao.deleteById(this.tema.getId());
+        assertNull(dao.read(this.tema.getId()));
         LogManager.getLogger(clazz).debug("Fin de testDeleteByID()");
     }
 
     @Test
     public void testFindAll() {
         LogManager.getLogger(clazz).debug("Inicio de testFindAll()");
-        dao.create(new Tema(2, "Tema2", "Pregunta2"));
-        dao.create(new Tema(3, "Tema3", "Pregunta3"));
+        dao.create(new Tema("Tema2", "Pregunta2"));
+        dao.create(new Tema("Tema3", "Pregunta3"));
         assertEquals(3, dao.findAll().size());
         LogManager.getLogger(clazz).debug("Fin de testFindAll()");
     }
 
-    
     @After
     public void after() {
-        LogManager.getLogger(clazz).debug(
-                "Limpiando tablas afectadas");
+        LogManager.getLogger(clazz).debug("Limpiando tablas afectadas");
         dao.deleteAll();
     }
-    
+
 }
