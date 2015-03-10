8b602d97fb6dc8b2926b9a0d8d8a832d8964b88d
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-11 00:41:43 +0100

    Update IVotacionDao/VotacionDao con métodos para calcular media, borrar por tema y consultar por tema

--- src/main/java/es/upm/miw/models/daos/IVotacionDao.java
+++ src/main/java/es/upm/miw/models/daos/IVotacionDao.java
@@ -1,7 +1,17 @@
 package es.upm.miw.models.daos;
 
+import java.util.List;
+
+import es.upm.miw.models.entities.Tema;
 import es.upm.miw.models.entities.Votacion;
+import es.upm.miw.models.utils.NivelEstudio;
 
 public interface IVotacionDao extends IGenericDao<Votacion, Integer> {
+    
+    public List<Votacion> findAllByTema(Tema tema);
+    
+    public int deleteAllByTema(Tema tema);
+    
+    public double valoracionMediaByNivelEstudio(NivelEstudio nivelEstudio);
 
 }
--- src/main/java/es/upm/miw/models/daos/jpa/VotacionDaoJpa.java
+++ src/main/java/es/upm/miw/models/daos/jpa/VotacionDaoJpa.java
@@ -1,7 +1,16 @@
 package es.upm.miw.models.daos.jpa;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.apache.logging.log4j.LogManager;
+
 import es.upm.miw.models.daos.IVotacionDao;
+import es.upm.miw.models.entities.Tema;
 import es.upm.miw.models.entities.Votacion;
+import es.upm.miw.models.utils.NivelEstudio;
 
 public class VotacionDaoJpa extends GenericDaoJpa<Votacion, Integer> implements IVotacionDao {
 
@@ -14,4 +23,43 @@ public class VotacionDaoJpa extends GenericDaoJpa<Votacion, Integer> implements 
         return Votacion.TABLE;
     }
 
+    private static final String FIND_VOTACION_BY_TEMA = "SELECT v FROM Votacion v WHERE v.tema = :tema ";
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<Votacion> findAllByTema(Tema tema) {
+        Query query = DaoJpaFactory.getEntityManagerFactory().createEntityManager()
+                .createQuery(FIND_VOTACION_BY_TEMA);
+        query.setParameter("tema", tema);
+        return (List<Votacion>) query.getResultList();
+    }
+
+    private static final String DELETE_VOTACION_BY_TEMA = "DELETE FROM Votacion v WHERE v.tema = :tema ";
+
+    @Override
+    public int deleteAllByTema(Tema tema) {
+        EntityManager entityManager = DaoJpaFactory.getEntityManagerFactory().createEntityManager();
+        int deletedCount = -1;
+        try {
+            entityManager.getTransaction().begin();
+            deletedCount = entityManager.createQuery(DELETE_VOTACION_BY_TEMA).setParameter("tema", tema).executeUpdate();
+            entityManager.getTransaction().commit();
+            LogManager.getLogger(GenericDaoJpa.class).debug(" rows deleteByTema: " + deletedCount);
+        } catch (Exception e) {
+            LogManager.getLogger(GenericDaoJpa.class).error("delete: " + e);
+            if (entityManager.getTransaction().isActive())
+                entityManager.getTransaction().rollback();
+        } finally {
+            entityManager.close();
+        }
+
+        return deletedCount;
+
+    }
+
+    @Override
+    public double valoracionMediaByNivelEstudio(NivelEstudio nivelEstudio) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 }
