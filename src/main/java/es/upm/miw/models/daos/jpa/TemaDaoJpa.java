196516bf78ee8c077dc4af316766918f74691b4f
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-11 13:44:28 +0100

    Initial Update TemaDaoJpa

--- src/main/java/es/upm/miw/models/daos/jpa/TemaDaoJpa.java
+++ src/main/java/es/upm/miw/models/daos/jpa/TemaDaoJpa.java
@@ -1,12 +1,29 @@
 package es.upm.miw.models.daos.jpa;
 
+import es.upm.miw.models.daos.DaoFactory;
 import es.upm.miw.models.daos.ITemaDao;
+import es.upm.miw.models.daos.IVotacionDao;
 import es.upm.miw.models.entities.Tema;
 
-public class TemaDaoJpa extends GenericDaoJpa<Tema, Integer> implements ITemaDao{
-    
+public class TemaDaoJpa extends GenericDaoJpa<Tema, Integer> implements ITemaDao {
+
     public TemaDaoJpa() {
         super(Tema.class);
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        Tema tema = this.read(id);
+        if (tema != null) {
+            this.deleteRelatedVotaciones(tema);
+            super.deleteById(id);
+        }
+    }
+
+    private void deleteRelatedVotaciones(Tema tema) {
+        DaoFactory.setFactory(new DaoJpaFactory());
+        IVotacionDao votacionDao = DaoFactory.getFactory().getVotacionDao();
+        votacionDao.deleteAllByTema(tema);
     }
 
     @Override
