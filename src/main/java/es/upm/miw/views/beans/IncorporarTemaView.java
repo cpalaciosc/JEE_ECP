ff875ae4456c713a5f14e97cc0bf67c780a48b3c
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-12 19:32:51 +0100

    Update IncorporarTemaView

--- src/main/java/es/upm/miw/views/beans/IncorporarTemaView.java
+++ src/main/java/es/upm/miw/views/beans/IncorporarTemaView.java
@@ -1,5 +1,39 @@
 package es.upm.miw.views.beans;
 
-public class IncorporarTemaView {
+import org.apache.logging.log4j.LogManager;
+
+import es.upm.miw.controllers.IIncorporarTemaController;
+
+public class IncorporarTemaView extends TemasBean {
+
+    private final static Class<IncorporarTemaView> clazz = IncorporarTemaView.class;
+
+    public IncorporarTemaView() {
+        super();
+    }
+
+    private static final String INCORPORACION_OK = "Se ha registrado el tema satisfactoriamente.";
+
+    private static final String INCORPORACION_FAIL = "No se ha podido registrar el tema.";
+
+    public String incorporarTema() {
+        IIncorporarTemaController incorporarTemaController = this.getControllerFactory()
+                .getIncorporarTemaController();
+        boolean isCreated = incorporarTemaController.incorporarTema(this.tema);
+        String next = null;
+        if(isCreated){
+            this.setErrorMsg(INCORPORACION_OK);
+            next = "home";
+        }
+        else{
+            this.setErrorMsg(INCORPORACION_FAIL);
+            next = "tema/IncorporarTema";
+        }
+
+        LogManager.getLogger(clazz).debug("Creación de tema " + tema + " resultado " + isCreated + " Proxima vista "+next);
+
+        return next;
+
+    }
 
 }
