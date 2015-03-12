2452bc6358e7d2306ed91cbbd97abdb2b37166d0
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-12 19:33:58 +0100

    Update IncorporarTemaController

--- src/main/java/es/upm/miw/controllers/IIncorporarTemaController.java
+++ src/main/java/es/upm/miw/controllers/IIncorporarTemaController.java
@@ -1,5 +1,9 @@
 package es.upm.miw.controllers;
 
+import es.upm.miw.models.entities.Tema;
+
 public interface IIncorporarTemaController {
+    
+    public boolean incorporarTema(Tema tema);
 
 }
--- src/main/java/es/upm/miw/controllers/ejbs/IncorporarTemaController.java
+++ src/main/java/es/upm/miw/controllers/ejbs/IncorporarTemaController.java
@@ -1,7 +1,22 @@
 package es.upm.miw.controllers.ejbs;
 
+import org.apache.logging.log4j.LogManager;
+
 import es.upm.miw.controllers.IIncorporarTemaController;
+import es.upm.miw.models.daos.DaoFactory;
+import es.upm.miw.models.daos.ITemaDao;
+import es.upm.miw.models.entities.Tema;
 
 public class IncorporarTemaController extends ControllerEjb implements IIncorporarTemaController {
+    
+    private final static Class<IncorporarTemaController> clazz = IncorporarTemaController.class;
+
+    @Override
+    public boolean incorporarTema(Tema tema) {
+        ITemaDao temaDao = DaoFactory.getFactory().getTemaDao();
+        temaDao.create(tema);
+        LogManager.getLogger(clazz).debug("Tema creado con id "+tema.getId());
+        return tema.getId()!=null ? true : false;
+    }
 
 }
