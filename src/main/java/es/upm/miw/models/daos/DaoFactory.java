479e3bcf9c3fe0b4ae000217bffaf765f749b8c4
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-13 19:31:08 +0100

    Upate DaoFactory

--- src/main/java/es/upm/miw/models/daos/DaoFactory.java
+++ src/main/java/es/upm/miw/models/daos/DaoFactory.java
@@ -1,14 +1,18 @@
 package es.upm.miw.models.daos;
 
+import es.upm.miw.models.daos.jpa.DaoJpaFactory;
+
 public abstract class DaoFactory {
     public static DaoFactory factory = null;
 
-    public static void setFactory(DaoFactory factory) { 
+    public static void setFactory(DaoFactory factory) {
         DaoFactory.factory = factory;
     }
 
     public static DaoFactory getFactory() {
-        assert factory != null;
+        if (factory == null) {
+            factory = new DaoJpaFactory();
+        }
         return factory;
     }
 
