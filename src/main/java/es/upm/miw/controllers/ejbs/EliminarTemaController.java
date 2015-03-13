12233e9b4b1bdd7318aae2f859794b509977d608
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-13 19:30:33 +0100

    Update EliminarTemaController

--- src/main/java/es/upm/miw/controllers/ejbs/EliminarTemaController.java
+++ src/main/java/es/upm/miw/controllers/ejbs/EliminarTemaController.java
@@ -1,7 +1,29 @@
 package es.upm.miw.controllers.ejbs;
 
+import java.util.List;
+
 import es.upm.miw.controllers.IEliminarTemaController;
+import es.upm.miw.models.entities.Tema;
 
 public class EliminarTemaController extends ControllerEjb implements IEliminarTemaController {
+
+    @Override
+    public List<Tema> consultarTemas() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean eliminarTema() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+    public static final String CODIGO_SEGURIDAD = "666";
+
+    @Override
+    public boolean autorizar(String codigoSeguridad) {
+        return codigoSeguridad.equals(CODIGO_SEGURIDAD);
+    }
 
 }
