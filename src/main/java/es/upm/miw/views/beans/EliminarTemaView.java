a59647da4702e98821a8f66f3de383d79215c248
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-13 19:28:53 +0100

    Add EliminarTemaView

--- src/main/java/es/upm/miw/views/beans/EliminarTemaView.java
+++ src/main/java/es/upm/miw/views/beans/EliminarTemaView.java
@@ -1,5 +1,15 @@
 package es.upm.miw.views.beans;
 
-public class EliminarTemaView {
+public class EliminarTemaView extends TemasBean {
 
+    private final static Class<EliminarTemaView> clazz = EliminarTemaView.class;
+
+    public EliminarTemaView() {
+        super();
+    }
+    
+    public String eliminarTema(){
+        return null;
+    }
+    
 }
