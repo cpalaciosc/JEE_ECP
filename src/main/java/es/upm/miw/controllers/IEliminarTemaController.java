d16a84ec86ee95363c715020446b5bc99f566d5f
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-13 19:29:44 +0100

    Update IEliminarTemaController

--- src/main/java/es/upm/miw/controllers/IEliminarTemaController.java
+++ src/main/java/es/upm/miw/controllers/IEliminarTemaController.java
@@ -1,5 +1,15 @@
 package es.upm.miw.controllers;
 
+import java.util.List;
+
+import es.upm.miw.models.entities.Tema;
+
 public interface IEliminarTemaController {
+    
+    public boolean autorizar(String codigoSeguridad);
+
+    public List<Tema> consultarTemas();
+
+    public boolean eliminarTema();
 
 }
