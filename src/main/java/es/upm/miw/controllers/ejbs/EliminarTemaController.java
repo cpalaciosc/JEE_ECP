55d50921d5469c9917f4a7c6b2cca9c4c82b3b83
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-13 20:06:32 +0100

    Update EliminarTemaController

--- src/main/java/es/upm/miw/controllers/ejbs/EliminarTemaController.java
+++ src/main/java/es/upm/miw/controllers/ejbs/EliminarTemaController.java
@@ -2,23 +2,31 @@ package es.upm.miw.controllers.ejbs;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+
 import es.upm.miw.controllers.IEliminarTemaController;
+import es.upm.miw.models.daos.DaoFactory;
+import es.upm.miw.models.daos.ITemaDao;
 import es.upm.miw.models.entities.Tema;
 
 public class EliminarTemaController extends ControllerEjb implements IEliminarTemaController {
 
+    private final static Class<EliminarTemaController> clazz = EliminarTemaController.class;
+
     @Override
     public List<Tema> consultarTemas() {
-        // TODO Auto-generated method stub
-        return null;
+        ITemaDao temaDao = DaoFactory.getFactory().getTemaDao();
+        List<Tema> result = temaDao.findAll();
+        LogManager.getLogger(clazz).debug("Consultando los temas " + result.size());
+        return result;
     }
 
     @Override
     public boolean eliminarTema() {
-        // TODO Auto-generated method stub
+
         return false;
     }
-    
+
     public static final String CODIGO_SEGURIDAD = "666";
 
     @Override
