5e8ccc7d6215b3168c0573b8d93629225197d96b
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-14 17:47:39 +0100

    Update VerVotacionesController

--- src/main/java/es/upm/miw/controllers/ejbs/VerVotacionesController.java
+++ src/main/java/es/upm/miw/controllers/ejbs/VerVotacionesController.java
@@ -1,7 +1,31 @@
 package es.upm.miw.controllers.ejbs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import es.upm.miw.controllers.IVerVotacionesController;
+import es.upm.miw.models.daos.DaoFactory;
+import es.upm.miw.models.daos.ITemaDao;
+import es.upm.miw.models.daos.IVotacionDao;
+import es.upm.miw.models.entities.Tema;
+import es.upm.miw.models.utils.TemaValoracionMedia;
+import es.upm.miw.models.utils.ValoracionMedia;
 
 public class VerVotacionesController extends ControllerEjb implements IVerVotacionesController {
 
+    @Override
+    public List<TemaValoracionMedia> obtenerValoracionesMediaPorTema() {
+        IVotacionDao votacionDao = DaoFactory.getFactory().getVotacionDao();
+        ITemaDao temaDao = DaoFactory.getFactory().getTemaDao();
+        List<Tema> listTemas = temaDao.findAll();
+        List<TemaValoracionMedia> listTemaValoracionMedias = new ArrayList<TemaValoracionMedia>();
+        for(Tema tmpTema : listTemas){
+            List<ValoracionMedia> valoracionMedia = votacionDao.valoracionMediaByNivelEstudio(tmpTema);
+            TemaValoracionMedia tmp = new TemaValoracionMedia();
+            tmp.setTema(tmpTema);
+            tmp.setValoracionMedia(valoracionMedia);
+            listTemaValoracionMedias.add(tmp);
+        }  
+        return listTemaValoracionMedias;
+    }
 }
