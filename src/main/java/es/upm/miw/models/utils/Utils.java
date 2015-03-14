f354da199ff0655e4e88ef6e99367b3c08b6f749
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-14 17:46:54 +0100

    Update Utils

--- src/main/java/es/upm/miw/models/utils/Utils.java
+++ src/main/java/es/upm/miw/models/utils/Utils.java
@@ -1,12 +1,43 @@
 package es.upm.miw.models.utils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 public class Utils {
 
     public static List<NivelEstudio> getNivelEstudioList() {
         return Arrays.asList(NivelEstudio.values());
     }
+    
+    public static List<Integer> getValoracionesList(){
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i=1; i<=10; i++){
+            list.add(i);
+        }
+        return list;
+    }
+    
+    public static String getClientIpAddr(HttpServletRequest request) {  
+        String ip = request.getHeader("X-Forwarded-For");  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("Proxy-Client-IP");  
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("WL-Proxy-Client-IP");  
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("HTTP_CLIENT_IP");  
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");  
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getRemoteAddr();  
+        }  
+        return ip;  
+    }    
 
 }
