dabf766f7a67786feea632bb0f8d35270f179b2c
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-14 14:51:14 +0100

    Add Utils

---
+++ src/main/java/es/upm/miw/models/utils/Utils.java
@@ -0,0 +1,12 @@
+package es.upm.miw.models.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Utils {
+
+    public static List<NivelEstudio> getNivelEstudioList() {
+        return Arrays.asList(NivelEstudio.values());
+    }
+
+}
