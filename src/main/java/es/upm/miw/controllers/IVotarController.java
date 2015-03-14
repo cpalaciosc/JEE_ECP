2906bc09cb165b59d9ac3a7987c8f92b5821cd4a
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-14 15:59:31 +0100

    Update IVotarController

--- src/main/java/es/upm/miw/controllers/IVotarController.java
+++ src/main/java/es/upm/miw/controllers/IVotarController.java
@@ -1,5 +1,16 @@
 package es.upm.miw.controllers;
 
+import java.util.List;
+
+import es.upm.miw.models.entities.Tema;
+import es.upm.miw.models.entities.Votacion;
+
 public interface IVotarController {
+    
+    public List<Tema> consultarTemas();
+    
+    public Tema consultarTema(int idTema);
+    
+    public boolean votar(Votacion votacion);
 
 }
