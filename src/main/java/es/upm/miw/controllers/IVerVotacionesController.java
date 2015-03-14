a6394b0fde96c0e45a3a5718566521b3a82d0709
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-14 17:47:52 +0100

    Update IVerVotacionesController

--- src/main/java/es/upm/miw/controllers/IVerVotacionesController.java
+++ src/main/java/es/upm/miw/controllers/IVerVotacionesController.java
@@ -1,5 +1,11 @@
 package es.upm.miw.controllers;
 
+import java.util.List;
+
+import es.upm.miw.models.utils.TemaValoracionMedia;
+
 public interface IVerVotacionesController {
+
+    public List<TemaValoracionMedia> obtenerValoracionesMediaPorTema();
 
 }
