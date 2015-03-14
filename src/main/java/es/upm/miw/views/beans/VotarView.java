f5d2695a196f60ad10aa7b56f642eb170a535aa2
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-14 14:53:30 +0100

    Update VotarView

--- src/main/java/es/upm/miw/views/beans/VotarView.java
+++ src/main/java/es/upm/miw/views/beans/VotarView.java
@@ -1,5 +1,37 @@
 package es.upm.miw.views.beans;
 
-public class VotarView {
+import es.upm.miw.controllers.IVotarController;
+import es.upm.miw.models.entities.Votacion;
+
+public class VotarView extends TemasBean {
+
+    private Votacion votacion;
+
+    private int idTema;
+    
+    public VotarView() {
+        this.votacion = new Votacion();
+    }
+
+    public Votacion getVotacion() {
+        return votacion;
+    }
+
+    public void setVotacion(Votacion votacion) {
+        this.votacion = votacion;
+    }
+
+    public int getIdTema() {
+        return idTema;
+    }
+
+    public void setIdTema(int idTema) {
+        this.idTema = idTema;
+    }
+
+    public void prepararVotacion() {
+        IVotarController votarController = this.getControllerFactory().getVotarController();
+        tema = votarController.consultarTema(idTema);
+    }
 
 }
