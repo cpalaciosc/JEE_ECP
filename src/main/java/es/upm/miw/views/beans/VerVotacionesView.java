9baade74b5b920ed5f460b454ac48bd8ffe78747
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-14 17:48:27 +0100

    Update VerVotacionesView

--- src/main/java/es/upm/miw/views/beans/VerVotacionesView.java
+++ src/main/java/es/upm/miw/views/beans/VerVotacionesView.java
@@ -1,5 +1,32 @@
 package es.upm.miw.views.beans;
 
-public class VerVotacionesView {
+import java.util.List;
+
+import es.upm.miw.controllers.IVerVotacionesController;
+import es.upm.miw.models.utils.TemaValoracionMedia;
+
+public class VerVotacionesView extends ViewBean {
+
+    private List<TemaValoracionMedia> temaValoracionMediaList;
+
+    public VerVotacionesView() {
+
+    }
+
+    public List<TemaValoracionMedia> getTemaValoracionMediaList() {
+        return temaValoracionMediaList;
+    }
+
+    public void setTemaValoracionMediaList(List<TemaValoracionMedia> temaValoracionMediaList) {
+        this.temaValoracionMediaList = temaValoracionMediaList;
+    }
+
+    public String generarReporte() {
+        IVerVotacionesController verVotacionesController = this.getControllerFactory()
+                .getVerVotacionesController();
+        temaValoracionMediaList = verVotacionesController.obtenerValoracionesMediaPorTema();
+        return "votacion/ver";
+
+    }
 
 }
