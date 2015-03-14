9a533a1f65b318a68b9d2d77b64fa87d417361c0
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-14 13:16:25 +0100

    Update ControllerEjb

--- src/main/java/es/upm/miw/controllers/ejbs/ControllerEjb.java
+++ src/main/java/es/upm/miw/controllers/ejbs/ControllerEjb.java
@@ -1,5 +1,22 @@
 package es.upm.miw.controllers.ejbs;
 
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+
+import es.upm.miw.models.daos.DaoFactory;
+import es.upm.miw.models.daos.ITemaDao;
+import es.upm.miw.models.entities.Tema;
+
 public abstract class ControllerEjb {
+
+    private final static Class<ControllerEjb> clazz = ControllerEjb.class;
+
+    protected List<Tema> consultarTemas() {
+        ITemaDao temaDao = DaoFactory.getFactory().getTemaDao();
+        List<Tema> result = temaDao.findAll();
+        LogManager.getLogger(clazz).debug("Consultando los temas " + result.size());
+        return result;
+    }
 
 }
