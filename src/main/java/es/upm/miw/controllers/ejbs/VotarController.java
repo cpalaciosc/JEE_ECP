b005150a8f77cc03ce00ebc8fc58a73b8c719fc7
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-14 13:16:40 +0100

    Update VotarController

--- src/main/java/es/upm/miw/controllers/ejbs/VotarController.java
+++ src/main/java/es/upm/miw/controllers/ejbs/VotarController.java
@@ -1,7 +1,15 @@
 package es.upm.miw.controllers.ejbs;
 
+import java.util.List;
+
 import es.upm.miw.controllers.IVotarController;
+import es.upm.miw.models.entities.Tema;
 
 public class VotarController extends ControllerEjb implements IVotarController {
+
+    @Override
+    public List<Tema> consultarTemas() {
+        return super.consultarTemas();
+    }
 
 }
