465cebe413ea314a9b4009c9589ef906c62ed2c1
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-18 11:14:44 +0100

    Update TemaResoruce

--- src/main/java/es/upm/miw/ws/rest/TemaResource.java
+++ src/main/java/es/upm/miw/ws/rest/TemaResource.java
@@ -3,18 +3,19 @@ package es.upm.miw.ws.rest;
 import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.apache.logging.log4j.LogManager;
 
-import es.upm.miw.controllers.IEliminarTemaController;
-import es.upm.miw.controllers.IIncorporarTemaController;
-import es.upm.miw.controllers.ejbs.ControllerEjbFactory;
+import es.upm.miw.models.daos.DaoFactory;
+import es.upm.miw.models.daos.ITemaDao;
 import es.upm.miw.models.entities.Tema;
 import es.upm.miw.ws.TemaUris;
 
@@ -24,26 +25,32 @@ public class TemaResource {
 
     private final static Class<TemaResource> clazz = TemaResource.class;
 
-    private ControllerEjbFactory controllerFactory = new ControllerEjbFactory();
-
     @POST
     @Consumes({MediaType.APPLICATION_XML})
     @Produces({MediaType.APPLICATION_XML})
     public Response create(Tema tema) {
         LogManager.getLogger(clazz).debug("Llamada al metodo create " + tema);
-        IIncorporarTemaController incorporarTemaController = controllerFactory
-                .getIncorporarTemaController();
-        incorporarTemaController.incorporarTema(tema);
+        ITemaDao temaDao = DaoFactory.getFactory().getTemaDao();
+        temaDao.create(tema);
+        LogManager.getLogger(clazz).debug("Tema creado con id "+tema.getId());
         return Response.ok(tema).build();
     }
+    
+    public static final String CODIGO_SEGURIDAD = "666";
 
     @DELETE
     @Path(TemaUris.PATH_ID_PARAM)
-    public Response delete(@PathParam("id") Integer id) {
-        IEliminarTemaController eliminarTemaController = controllerFactory
-                .getEliminarTemaController();
-
-        eliminarTemaController.eliminarTema(id);
+    public Response delete(@PathParam("id") Integer id, @QueryParam("code") String code) {
+        if(code==null){
+            code = "";
+        }
+        if(!code.equals(CODIGO_SEGURIDAD)){
+            throw new NotAuthorizedException("No tiene permiso para eliminar un tema");
+        }
+        
+        ITemaDao temaDao = DaoFactory.getFactory().getTemaDao();
+        LogManager.getLogger(clazz).debug("Eliminando tema con id " + id);
+        temaDao.deleteById(id);
 
         return Response.ok().build();
     }
