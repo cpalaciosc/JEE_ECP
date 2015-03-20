0bbc51e620fccbb9299b2d3a5c6b0dbd7b5f949b
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-20 14:22:40 +0100

    Initial - Update VotacionesResource

--- src/main/java/es/upm/miw/ws/rest/VotacionesResource.java
+++ src/main/java/es/upm/miw/ws/rest/VotacionesResource.java
@@ -1,11 +1,20 @@
 package es.upm.miw.ws.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import es.upm.miw.models.daos.DaoFactory;
+import es.upm.miw.models.daos.ITemaDao;
+import es.upm.miw.models.daos.IVotacionDao;
+import es.upm.miw.models.entities.Tema;
+import es.upm.miw.models.utils.TemaValoracionMedia;
+import es.upm.miw.models.utils.ValoracionMedia;
 import es.upm.miw.ws.VotacionesUris;
 
 @Path(VotacionesUris.PATH_VOTACIONES)
@@ -14,7 +23,21 @@ public class VotacionesResource {
     @GET
     @Produces({MediaType.APPLICATION_XML})
     public Response consultar() {
-        return null;
+        IVotacionDao votacionDao = DaoFactory.getFactory().getVotacionDao();
+        ITemaDao temaDao = DaoFactory.getFactory().getTemaDao();
+        List<Tema> listTemas = temaDao.findAll();
+        List<TemaValoracionMedia> listTemaValoracionMedias = new ArrayList<TemaValoracionMedia>();
+        for (Tema tmpTema : listTemas) {
+            List<ValoracionMedia> valoracionMedia = votacionDao
+                    .valoracionMediaByNivelEstudio(tmpTema);
+            int numeroVotos = votacionDao.numeroVotos(tmpTema);
+            TemaValoracionMedia tmp = new TemaValoracionMedia();
+            tmp.setTema(tmpTema);
+            tmp.setValoracionMedia(valoracionMedia);
+            tmp.setNumeroVotos(numeroVotos);
+            listTemaValoracionMedias.add(tmp);
+        }
+        return Response.ok(listTemaValoracionMedias).build();
     }
 
 }
