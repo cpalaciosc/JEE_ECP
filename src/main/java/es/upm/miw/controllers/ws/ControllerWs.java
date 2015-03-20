2b19ccbb96c7ac9a9ed4ffbd4a8185fe2c96dce7
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-20 20:45:50 +0100

    Update ControllerWs

--- src/main/java/es/upm/miw/controllers/ws/ControllerWs.java
+++ src/main/java/es/upm/miw/controllers/ws/ControllerWs.java
@@ -1,5 +1,11 @@
 package es.upm.miw.controllers.ws;
 
+import java.util.List;
+
+import es.upm.miw.models.entities.Tema;
+import es.upm.miw.ws.TemaUris;
+import es.upm.miw.ws.utils.TemasWrapper;
+
 
 abstract class ControllerWs {
 
@@ -20,5 +26,13 @@ abstract class ControllerWs {
     protected static WsManager buildWebServiceManager(String... paths){
         return new WsManager(URI, paths);
     }
+    
+    public List<Tema> consultarTemas() {
+        WsManager wsManager = ControllerWs.buildWebServiceManager(TemaUris.PATH_TEMAS);
+        if (wsManager.read()) {
+            return wsManager.entity(TemasWrapper.class).getListTemas();
+        }
+        return null;
+    }    
 
 }
