4910b84ec6d319f585948dddbb43402722bc921d
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-20 20:31:01 +0100

    Add VerVotacionesController

---
+++ src/main/java/es/upm/miw/controllers/ws/VerVotacionesController.java
@@ -0,0 +1,16 @@
+package es.upm.miw.controllers.ws;
+
+import java.util.List;
+
+import es.upm.miw.controllers.IVerVotacionesController;
+import es.upm.miw.models.utils.TemaValoracionMedia;
+
+public class VerVotacionesController extends ControllerWs implements IVerVotacionesController {
+
+    @Override
+    public List<TemaValoracionMedia> obtenerValoracionesMediaPorTema() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
