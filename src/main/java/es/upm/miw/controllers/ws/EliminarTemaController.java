0ea1fb4752ac0d5b99f8eeba20b48c03be0939a6
cpalacios <c.palaciosc@alumnos.upm.es>
2015-03-20 22:04:53 +0100

    Update EliminarTemaController

--- src/main/java/es/upm/miw/controllers/ws/EliminarTemaController.java
+++ src/main/java/es/upm/miw/controllers/ws/EliminarTemaController.java
@@ -4,25 +4,32 @@ import java.util.List;
 
 import es.upm.miw.controllers.IEliminarTemaController;
 import es.upm.miw.models.entities.Tema;
+import es.upm.miw.ws.TemaUris;
 
 public class EliminarTemaController extends ControllerWs implements IEliminarTemaController {
 
     @Override
     public boolean autorizar(String codigoSeguridad) {
-        // TODO Auto-generated method stub
-        return false;
+        throw new UnsupportedOperationException("Operacion permitida via EJB");
     }
 
     @Override
     public List<Tema> consultarTemas() {
-        // TODO Auto-generated method stub
-        return null;
+        return super.consultarTemas();
     }
 
     @Override
     public boolean eliminarTema(int idTema) {
-        // TODO Auto-generated method stub
-        return false;
+        throw new UnsupportedOperationException("Operacion permitida via EJB");
+    }
+
+    @Override
+    public boolean eliminarTema(int idTema, String codigoSeguridad) {
+        WsManager wsManager = ControllerWs.buildWebServiceManager(TemaUris.PATH_TEMAS);
+        wsManager.addPath(TemaUris.PATH_ID_PARAM);
+        wsManager.addParams("code", codigoSeguridad);
+        wsManager.addPathParam("id", new Integer(idTema));
+        return wsManager.delete();
     }
 
 }
